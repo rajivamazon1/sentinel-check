80b7a0d8a5c6de62386a6bfb3d22b93c25fb9f3d
rajiv.mandal <rajivamazon1@gmail.com>
2024-04-13 03:21:57 +0530

    removed backend

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "myrmtbucket-abc-rkm"
-    key = "rajiv/terraform.tfstates"
-    region = "us-east-1"
-    dynamodb_table = "mytable_state_lock"
-  }
-}
-
