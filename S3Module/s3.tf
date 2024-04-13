cc755128562a6a2b2dc92c7f1a0a79879f1f23b1
rajiv.mandal <rajivamazon1@gmail.com>
2024-04-13 06:12:22 +0530

    removed s3

--- S3Module/s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "testbkt-1" {
-  bucket = "myawsbkt-rkm-pmnabc"
-
-}
--- main.tf
+++ main.tf
@@ -2,7 +2,3 @@ module "ec2instances" {
     source = "./Ec2Module"
   
 }
-
-module "s3bucket" {
-  source = "./S3Module"
-}
